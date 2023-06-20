8a7d96036885d86a232641abede024dfbadb937d
Isai Rincon <Rinconisai@gmail.com>
2023-06-19 19:56:35 -0700

    More affirmations, but bug where one is blank

--- main.js
+++ main.js
@@ -5,15 +5,16 @@ class Click extends Phaser.Scene{
 
     create(){
         const msgs = ["You're wonderful!", "You are loved!", "You still have time!", "You deserve to rest!", "You're more capable than you think!", "You are so, so strong!", "This will pass!", 
-                        "You are worthy!", "You are never alone!"];
+                        "You are worthy!", "You are never alone!", "You're priceless!", "You're a diamond in the rough!", "You will succeed!", 
+                        "Support is never that far away!"];
         this.w = this.game.config.width;
         this.h = this.game.config.height;
         this.cameras.main.setBackgroundColor('#ffb6c1');
         let greeting = this.add.text(this.w/9.6, this.h/2, "Click if you like. I really hope it helps.\nYou're doing the best you can and that's all that matters.").setFontSize(45);
         this.input.on('pointerdown', () =>{
-            greeting.text = msgs[Math.floor(Math.random() * 8)];
+            greeting.text = msgs[Math.floor(Math.random() * msgs.length-1)];
             greeting.x = this.w/3;
-        })
+        });
     }
 }
 
@@ -33,7 +34,5 @@ const game = new Phaser.Game({
             }
         }
     },
-   // backgroundColor: 0xffffff,
     scene: [Click],
-    //title: "Physics Demo"
 });
