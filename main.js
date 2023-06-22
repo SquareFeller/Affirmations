e36d8248375338fdb697cebc12f0f667bd11dc9a
Isai Rincon <Rinconisai@gmail.com>
2023-06-21 18:09:27 -0700

    More text tweaking

--- main.js
+++ main.js
@@ -6,15 +6,17 @@ class Click extends Phaser.Scene{
     create(){
         const msgs = ["You're wonderful!", "You are loved!", "You still have time!", "You deserve to rest!", "You're more capable than you think!", "You are so, so strong!", "This will pass!", 
                         "You are worthy!", "You are never alone!", "You're priceless!", "You're a diamond in the rough!", "You will succeed!", 
-                        "Support is never that far away!"];
+                        "Support is never that far away!", "You matter!"];
         this.w = this.game.config.width;
         this.h = this.game.config.height;
         this.cameras.main.setBackgroundColor('#ffb6c1');
         let greeting = this.add.text(this.w/9.9, this.h/2, "Click if you like. I really hope it helps.\nYou're doing the best you can and that's all that matters.").setFontSize(45).setTintFill('0x000000').setWordWrapWidth(this.w);
         this.input.on('pointerdown', () =>{
             greeting.text = msgs[Math.floor(Math.random() * (msgs.length))];
-            greeting.x = this.w/6;
-            greeting.setFontSize(75).setWordWrapWidth(window.innerWidth/3);
+           // greeting.x = this.w/6;
+            //greeting.setFontSize(75).setWordWrapWidth(window.innerWidth/3);
+            greeting.displayWidth = AVAIL_WIDTH;
+            greeting.scaleY = text.scaleX;
         });
     }
 }
